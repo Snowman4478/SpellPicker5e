5e71d92fc50c1748ccec2b4fc2539b9e3f28cc12
Snowman4478 <philippe4478@gmail.com>
2024-07-12 16:26:29 -0400

    Setting up routes and prepping for adding templates

---
+++ backend/app/routes.py
@@ -0,0 +1,28 @@
+from flask import render_template, url_for, flash, redirect
+from app import app
+from app.forms import CharCreateForm
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/char-create", methods=['GET', 'POST'])
+def charcreate():
+    form = CharCreateForm()
+    if form.validate_on_submit():
+        flash(f'Created Chracter!', 'success')
+        return redirect(url_for('created'))
+    return render_template('charcreate.html', title='Create Character', form=form)
+
+@app.route("/random-char")
+def randomchar():
+    return render_template('randomchar.html')
+
+@app.route("/created")
+def created():
+    return render_template('created.html')
