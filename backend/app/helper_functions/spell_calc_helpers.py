1da20b04f1bb0dcff8586216bc5c71c5785a3008
Snowman4478 <philippe4478@gmail.com>
2024-09-07 21:30:05 -0400

    Spells known calculator for each class

--- backend/app/helper_functions/spell_calc_helpers.py
+++ backend/app/helper_functions/spell_calc_helpers.py
@@ -49,3 +49,56 @@ def highest_spell_slot(class_type, level):
         raise ValueError(f'{class_type} is not a valid class')
     
     return highest_slot
+
+# Now to calculate the # of spells that each class will have in total 9excluding cantrips)
+
+def bard_numb_of_spells(level, ability_modifier):
+    bard_spells_known =  {1: 4, 2: 5, 3: 6, 4: 7, 5: 8, 6: 9, 7: 10, 8: 11, 9: 12, 10: 14,
+                          11: 15, 12: 15, 13: 16, 14: 18, 15: 19, 16: 19, 17: 20, 18: 22, 19: 22, 20: 22}
+    
+    return bard_spells_known[level]
+
+
+# classes that know the whole spell list of their class will get spells equal to the amount able to be prepared (level + spellcast modifier, default 5)
+
+def cleric_numb_of_spells(level, ability_modifier):
+    return max(level + ability_modifier, 1)
+
+def druid_numb_of_spells(level, ability_modifier):
+    return max(level + ability_modifier, 1)
+
+def paladin_numb_of_spells(level, ability_modifier):
+    return 0 if level == 1 else max((level // 2) + ability_modifier, 1)
+
+def ranger_numb_of_spells(level, ability_modifier):
+    return 0 if level == 1 else math.ceil(level / 2) + 1
+
+def sorceror_numb_of_spells(level, ability_modifier):
+    if level in range(1, 12):
+        sorceror_spells_known = level + 1
+    elif level in range(17, 21):
+        sorceror_spells_known = 15
+    else:
+        spell_dict = {12: 12, 13: 13, 14: 13, 15: 14, 16: 14}
+        sorceror_spells_known = spell_dict[level]
+    
+    return sorceror_spells_known
+
+def warlock_numb_of_spells(level, ability_modifier):
+    if level in range(1, 9):
+        return level + 1
+    else:
+        return math.floor((level-9)/2) + 10 
+
+def wizard_numb_of_spells(level, ability_modifier):
+    return (2*(level-1)) + 6
+
+
+# Now a dispatcher function to call the right one and to clean up code
+def num_of_spells_by_class(class_type, level, ability_modifier=5):
+
+    function_name = f"{class_type}_numb_of_spells"
+
+    func_to_call = globals().get(function_name)
+    
+    return func_to_call(level, ability_modifier)
